bb2370540c456d4d5d020bb3fb0ce6a2af5d0924
JoshuaArus <joshua.arus@gmail.com>
2018-04-11 14:29:45 +0200

    first version of unit test which check :
    xml config file reading
    parserFactory initialization + dynamic loading feature

---
+++ ConfigTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using LightNovelSniffer.Config;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LightNovelSniffer_Tests
+{
+    [TestClass]
+    public class ConfigTests
+    {
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            ConfigTools.InitConf();
+        }
+
+        [TestMethod]
+        public void VerifGlobals()
+        {
+            Assert.AreEqual(Globale.OUTPUT_FOLDER, "outputFolder");
+            Assert.IsTrue(Globale.INTERACTIVE_MODE);
+            Assert.AreEqual(Globale.PUBLISHER, "publisher");
+            Assert.AreEqual(Globale.DEFAULT_CHAPTER_TITLE, "defaultChapterTitle");
+        }
+
+        [TestMethod]
+        public void VerifLn()
+        {
+            Assert.IsNotNull(Globale.LN_TO_RETRIEVE);
+            Assert.AreEqual(Globale.LN_TO_RETRIEVE.Count, 1);
+
+            LnParameters ln = Globale.LN_TO_RETRIEVE.First();
+
+            Assert.AreEqual(ln.name, "lnName");
+            
+            Assert.IsNotNull(ln.authors);
+            Assert.AreEqual(ln.authors.Count, 2);
+            Assert.AreEqual(ln.authors[0], "author 1");
+            Assert.AreEqual(ln.authors[1], "author 2");
+
+            Assert.AreEqual(ln.urlCover, "http://google.com/image.jpg");
+
+            Assert.IsNotNull(ln.urlParameters);
+            Assert.AreEqual(ln.urlParameters.Count, 1);
+            Assert.AreEqual(ln.urlParameters[0].url, "http://google.com/Novel/chapter-{0}");
+            Assert.AreEqual(ln.urlParameters[0].language, "EN");
+            Assert.AreEqual(ln.urlParameters[0].firstChapterNumber, 1);
+            Assert.AreEqual(ln.urlParameters[0].lastChapterNumber, 10);
+        }
+    }
+}
---
+++ Parser/CustomParser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+using LightNovelSniffer.Web;
+using LightNovelSniffer.Web.Parser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LightNovelSniffer_Tests.Parser
+{
+    public class CustomParser : IParser
+    {
+        public LnChapter Parse(HtmlDocument doc)
+        {
+            List<HtmlNode> paragraphs = new List<HtmlNode>();
+            HtmlNode p = HtmlTextNode.CreateNode("innerText");
+            paragraphs.Add(p);
+            LnChapter c = new LnChapter("title", paragraphs);
+            c.chapNumber = 12;
+            return c;
+        }
+
+        public bool CanParse(string url)
+        {
+            return url.Contains("LightNovelSnifferTests");
+        }
+
+        public void test(LnChapter chapter)
+        {
+            Assert.AreEqual(chapter.title, "title");
+            Assert.AreEqual(chapter.paragraphs.Count, 1);
+            Assert.AreEqual(chapter.paragraphs.First().InnerText, "innerText");
+            Assert.AreEqual(chapter.chapNumber, 12);
+        }
+    }
+}
